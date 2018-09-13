13a16e9980099656168e93e6a8340920ef92d39a
JoelDadzie <42823973+JoelDadzie@users.noreply.github.com>
2018-09-13 01:54:37 +0100

    Add files via upload

---
+++ ass4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+  int gcd (int huge, int little)
+{
+ int result;
+ result=little%huge;
+ if (result==0)
+    {
+        return little; 
+    }
+  else
+   {
+    huge=little;
+    little=result;
+    gcd(little,huge);
+   }
+}
+int main()
+{
+cout<<"Calculation of the GCF of two numbers"<<endl;
+int large;
+int small;
+int newvar;
+cout<<"enter the large number"<<endl;
+cin>>large;
+cout<<"enter the small number"<<endl;
+cin>>small;
+newvar=gcd(large,small);
+cout<<newvar<<endl;
+
+system ("PAUSE");
+       return 0;
+}
